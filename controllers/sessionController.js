110f2280bf6da916776f44acda409ba9f07fbde0
Tracy Cano <tracycano3.14@gmail.com>
2025-06-18 17:19:53 -0700

    fixed the error where I could not see medication page appear

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -6,19 +6,28 @@ const registerShow = (req, res) => {
 };
 
 const registerDo = async (req, res, next) => {
+  console.log("register endpoint")
+  console.log(req.body)
   if (req.body.password != req.body.password1) {
     req.flash("error", "The passwords entered do not match.");
     return res.render("register", {  errors: req.flash("error") });
   }
   try {
-    const newUser = await User.create(req.body);
+    console.log("try create user")
+    
+    const newUser = await User.create({
+      username:req.body.name, 
+      email: req.body.email, 
+      password:req.body.password
+    });
     console.log('User created:', newUser.email); //take out when it works
 
     req.login(newUser, function(err){
       if (err) return next(err);
-      return res.direct("/");
+      return res.redirect("/");
     });
   } catch (e) {
+    console.log(`error ${e}`)
     if (e.constructor.name === "ValidationError") {
       parseVErr(e, req);
     } else if (e.name === "MongoServerError" && e.code === 11000) {
